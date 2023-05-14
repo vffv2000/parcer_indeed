10a37ba573e4a20f515f54f815613e93f645ebda
Valentino <90069922+vffv2000@users.noreply.github.com>
2023-05-15 00:22:18 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import json
+import time
+from art import tprint
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+tprint("@vffv2000")
+print("Contact on Telegram for custom script development")
+print('Welcome to the parser for the website https://ca.indeed.com/')
+print('To start the parser, type <link> in the console')
+
+#https://www.indeed.com/jobs?q=manager&l=chicago&fromage=1
+# https://www.indeed.com/jobs?q=manager&l=chicago&fromage=1&start=10
+def get_info(url):
+    driver = webdriver.Chrome()
+    driver.get(url)
+    time.sleep(5)
+    count_element = driver.find_element(By.CSS_SELECTOR, 'div.jobsearch-JobCountAndSortPane-jobCount span:first-child')
+    count_text = count_element.text
+    count = int(count_text.replace(',', '').split()[0])//10
+    print(count, " links detected")
+    if count > 650:
+        count = 650
+
+    with open('data.txt', 'a') as f:
+        for i in range(10, count, 10):
+            idx = url.index('&')
+            new_url = url[:idx] + f"&start={i}" + url[idx:]
+            f.write(json.dumps(new_url) + '\n')
+
+
+
+
+def main():
+    while True:
+        print("Enter your URL, for example (https://www.indeed.com/jobs?q=full+time&l=Charlotte%2C+NC&vjk=8cbba115fdef3efd)")
+        url_l = input("input:")
+        get_info(url_l)
+        print("All links have been saved to the 'data.txt' file located in the same directory as the 'find_all_links.exe' file.")
+        print(' -----------------------FINAL -------------------------------')
+
+
+
+if __name__ == "__main__":
+    main()
